14aa9ecaee20c079c275a8a1f65a22bbfe1f0879
Nicholas Masson <nick.masson@breakthroughenergy.org>
2024-03-07 09:58:47 -0700

    benchmark and example code for redis

--- spire-ingest-resample-worker/lib/scratch_cache.py
+++ spire-ingest-resample-worker/lib/scratch_cache.py
@@ -2,6 +2,11 @@ from stub import pubsub_message
 from lib.schemas import SpireWaypointRecords, WaypointCache, SpireWaypointPositional
 from uuid import UUID
 from datetime import datetime
+import redis
+from redis.retry import Retry
+from redis.backoff import ExponentialBackoff
+
+import json
 
 #  batch of flight instance waypoints from the spire-ingest-api-scraper
 flight_waypoints = SpireWaypointRecords.from_utf8_json(pubsub_message)
@@ -46,3 +51,43 @@ cache_entry = WaypointCache(
     key=f"spr:{flight_icao_addr}",
     waypoints=(w0_cache, w1_cache),
 )
+
+# gcloud compute instances create nick-tunnel --machine-type=f1-micro --zone=us-east1-b
+# REDIS_HOST=10.78.184.4 && gcloud compute ssh nick-tunnel --zone=us-east1-b -- -N -L 6379:$REDIS_HOST:6379
+
+redis_retry = Retry(ExponentialBackoff(), 3)
+redis_client = redis.Redis(
+    host="localhost",
+    port=6379,
+    retry=redis_retry,
+    retry_on_timeout=True,
+)
+# try writing single record w/ expiry as an atomic transaction
+transaction = redis_client.pipeline()
+transaction.hset(cache_entry.key, mapping=cache_entry.to_flatmap())
+transaction.expire(cache_entry.key, 10)
+transaction.execute()
+
+# redis_client.hget(cache_entry.key, "w1_flight_id")
+redis_client.hgetall(cache_entry.key)
+# redis_client.delete(cache_entry.key)
+
+
+expiry_sec = 5 * 60 * 60
+for i in range(60000):
+    # populate a bunch of phony records
+    phony_key = f"spr:{i}:06d"
+    transaction = redis_client.pipeline()
+    transaction.hset(phony_key, mapping=cache_entry.to_flatmap())
+    transaction.expire(phony_key, expiry_sec)
+    transaction.execute()
+
+    if not (i % 100):
+        info = redis_client.info()
+        print(f"key count: {info['db0']['keys']}")
+        print(f"{info['used_memory_human']} of {info['maxmemory_human']} used")
+
+print(redis_client.scan(count=100))
+
+info = redis_client.info()
+print(json.dumps(info, indent=4))
