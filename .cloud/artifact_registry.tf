ebdfc79bbfd3ea5a335bf23cae9e7e4a3d914984
Nick Masson <nick.masson@breakthroughenergy.org>
2024-03-11 09:42:24 -0600

    Update .cloud/artifact_registry.tf

--- .cloud/artifact_registry.tf
+++ .cloud/artifact_registry.tf
@@ -1,6 +1,6 @@
 resource "google_artifact_registry_repository" "flights_pipeline_dev" {
   location      = "us-east1"
-  repository_id = "flights-pipeline-dev"
+  repository_id = "flights-pipeline"
   description   = "flights-pipeline docker images"
   format        = "DOCKER"
 }
