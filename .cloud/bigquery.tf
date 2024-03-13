119c3b97dca6cf07b8926610951826238bb2ef09
Nick Masson <nick.masson@breakthroughenergy.org>
2024-03-13 09:00:44 -0600

    add bq tables & pubsub integration (#26)
    * add bq tables & pubsub integration
    
    * lint; \n

---
+++ .cloud/bigquery.tf
@@ -0,0 +1,31 @@
+resource "google_bigquery_dataset" "flights-pipeline-dev" {
+  dataset_id = "flights_pipeline_dev"
+  friendly_name = "[DEV] flights pipeline"
+  description = "data lake for observation flights data & derived data products"
+  location = "US"
+  delete_contents_on_destroy = false
+}
+
+resource "google_bigquery_dataset" "flights-pipeline-prod" {
+  dataset_id = "flights_pipeline_prod"
+  friendly_name = "[PROD] flights pipeline"
+  description = "data lake for observation flights data & derived data products"
+  location = "US"
+  delete_contents_on_destroy = false
+}
+
+resource "google_bigquery_table" "spire-flights-resampled-dev" {
+  dataset_id = google_bigquery_dataset.flights-pipeline-dev.dataset_id
+  table_id   = "spire_flights_resampled_dev"
+  friendly_name = "[DEV] spire flights, resampled"
+  description = "resampled and interpolated flight instances from the Spire API"
+  deletion_protection = true
+  time_partitioning {
+    field = "timestamp"
+    type = "DAY"
+  }
+  schema = file("${path.module}/schemas/bq_spire_flights_resampled.json")
+  depends_on = [
+    google_bigquery_dataset.flights-pipeline-dev,
+  ]
+}
