e163c57c1ff425d1a74ec41e53f9ab7f63c29bb6
Ben Fasoli <benfasoli@gmail.com>
2024-03-21 17:41:55 +0100

    Namespace PubSub ordering key with service prefix (#73)

--- spire-ingest-api-scraper/main.py
+++ spire-ingest-api-scraper/main.py
@@ -167,7 +167,7 @@ def main(
             )
 
             data = dto.as_utf8_json()
-            ordering_key = str(icao_address)
+            ordering_key = f"api-scraper:{icao_address}"
             queue_client.publish_async(data, ordering_key)
 
         queue_client.wait_for_publish()
