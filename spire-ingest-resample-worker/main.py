693b94d86cc2e76c9a3cb027f2eab7590a8ddf04
Nick Masson <nick.masson@breakthroughenergy.org>
2024-03-21 14:11:57 -0600

    sleep resampler if queue is empty (#77)
    * add sleep
    
    * relax pubsub ack and pull timeout; fix bad cloud logger handling on escape chars

--- spire-ingest-resample-worker/main.py
+++ spire-ingest-resample-worker/main.py
@@ -1,6 +1,7 @@
 """Entrypoint for the Spire Ingest Resample Worker."""
 
 import sys
+import time
 
 from datetime import datetime
 
@@ -42,6 +43,12 @@ def run():
         env.SPIRE_INGEST_WAYPOINTS_SUBSCRIPTION_ID
     ) as job_handler:
         job: SpireWaypointsRecord = job_handler.fetch()
+        if not job:
+            # if the queue is empty -> we get back [], then pause before retry
+            logger.info("job empty. sleeping... ")
+            time.sleep(10)
+            return
+
         logger.info(
             f"got job with {len(job.records)} records. "
             f"icao_address: {job.flight_info.icao_address}"
@@ -65,12 +72,12 @@ def run():
         # cases where we don't process the batch window received from pubsub
         try:
             validation_handler = ValidationHandler(cached, job)
-            validated_cache: list[SpireWaypointPositional] = (
-                validation_handler.cached_records
-            )
-            validated_records: list[SpireWaypointPositional] = (
-                validation_handler.records
-            )
+            validated_cache: list[
+                SpireWaypointPositional
+            ] = validation_handler.cached_records
+            validated_records: list[
+                SpireWaypointPositional
+            ] = validation_handler.records
             validated_flight_info: SpireFlightInfo | None = (
                 validation_handler.flight_info
             )
@@ -80,7 +87,7 @@ def run():
                 f"cache and/or records invalid. "
                 f"not processing batch. "
                 f"icao_address: {job.flight_info.icao_address} "
-                f"cache: {cached}. job: {job}."
+                f"job: {job}"
                 f"traceback: {format_traceback()}"
             )
             job_handler.ack()
@@ -91,7 +98,7 @@ def run():
                 f"and flight_id could not be inferred. "
                 f"not processing batch."
                 f"icao_address: {job.flight_info.icao_address} "
-                f"cache: {cached}. job: {job}"
+                f"job: {job}"
             )
             job_handler.ack()
             return
@@ -100,7 +107,7 @@ def run():
                 f"cache & records don't span more than 1 minute. "
                 f"icao_address: {job.flight_info.icao_address} "
                 f"updating cache. no export of records. "
-                f"cache: {cached}. job: {job}"
+                f"job: {job}"
             )
             new_cache_wpts: list[SpireWaypointPositional] = []
             if validated_cache:
@@ -125,15 +132,15 @@ def run():
         try:
             transform_handler = ResampleHandler(validated_cache, validated_records)
             transform_handler.interpolate()
-            resampled_records: list[SpireWaypointPositional] = (
-                transform_handler.waypoints_resampled
-            )
+            resampled_records: list[
+                SpireWaypointPositional
+            ] = transform_handler.waypoints_resampled
         except Exception:
             logger.error(
                 f"failed to interpolate."
                 f"not updating cache. not exporting records."
                 f"icao_address: {job.flight_info.icao_address} "
-                f"cache: {cached}. job: {job}"
+                f"job: {job}"
                 f"traceback: {format_traceback()}"
             )
             return
