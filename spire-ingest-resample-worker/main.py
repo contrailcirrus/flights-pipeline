109472ce9fdfdbda709c0a2cca7aefe486cdcc76
Nick Masson <nick.masson@breakthroughenergy.org>
2024-03-21 10:32:46 -0600

    update logging (#74)
    * update logging
    
    * roll back nack method; no explicit nack available for sub clients
    
    * lease ack log

--- spire-ingest-resample-worker/main.py
+++ spire-ingest-resample-worker/main.py
@@ -38,43 +38,30 @@ def run():
     cache_handler = CacheHandler(env.REDIS_HOST, env.REDIS_PORT)
     bq_publish_handler = PubSubPublishHandler(env.SPIRE_WAYPOINTS_BIGQUERY_TOPIC_ID)
 
-    logger.info(f"fetching record from {env.SPIRE_INGEST_WAYPOINTS_SUBSCRIPTION_ID}")
     with PubSubSubscriptionHandler(
         env.SPIRE_INGEST_WAYPOINTS_SUBSCRIPTION_ID
     ) as job_handler:
         job: SpireWaypointsRecord = job_handler.fetch()
-        logger.info(f"got job with {len(job.records)} records. job: {job}")
+        logger.info(
+            f"got job with {len(job.records)} records. "
+            f"icao_address: {job.flight_info.icao_address}"
+        )
 
-        logger.info(f"fetching from cache to {env.REDIS_HOST}:{env.REDIS_PORT}")
         try:
             cached: list[WaypointCache.Waypoint] = cache_handler.pull(
                 cache_key_fmt.format(icao_address=job.flight_info.icao_address)
             )
         except Exception:
             logger.error(
-                f"failed to fetch record from cache. exiting... "
+                f"error fetching record(s) from cache. exiting... "
                 f"traceback: {format_traceback()}"
             )
-            # TODO: choose preference here
-            # either exit w. code 1 (avoiding thrashing logs), so we have error level logs on the
-            # k8s infra log-stream
-            # - or - perpetual backoff and wait to parent loop, relying on error-level alert in
-            # application/service log stream
             sys.exit(1)
 
         if cached:
-            logger.info(
-                f"cache hit. found {len(cached)} prior waypoints "
-                f"for icao_address {job.flight_info.icao_address}, "
-                f"at {datetime.fromtimestamp(cached[-1]['timestamp']).isoformat()}."
-                f"interpolating until {job.records[0].timestamp}. cache: {cached}"
-            )
+            logger.info(f"cache hit: {len(cached)} waypoints")
         else:
-            logger.info(
-                f"cache miss. no prior waypoint(s) found for icao_address "
-                f"{job.flight_info.icao_address} at {job.records[0].timestamp}"
-            )
-
+            logger.info("cache miss.")
         # cases where we don't process the batch window received from pubsub
         try:
             validation_handler = ValidationHandler(cached, job)
@@ -91,8 +78,8 @@ def run():
         except Exception:
             logger.warning(
                 f"cache and/or records invalid. "
-                f"not processing batch with icao_address {job.flight_info.icao_address} "
-                f"and timestamp {job.records[0].timestamp}. "
+                f"not processing batch."
+                f"cache: {cached}. job: {job}."
                 f"traceback: {format_traceback()}"
             )
             job_handler.ack()
@@ -101,16 +88,15 @@ def run():
             logger.warning(
                 f"no flight_id available in records batch, "
                 f"and flight_id could not be inferred. "
-                f"not processing batch with icao_address {job.flight_info.icao_address} "
-                f"and timestamp {job.records[0].timestamp}."
+                f"not processing batch."
+                f"cache: {cached}. job: {job}"
             )
             job_handler.ack()
             return
         if not validated_gt_1min_span:
             logger.info(
-                f"no cache present and records don't span more than 1 minute. "
-                f"updating cache for icao_address {job.flight_info.icao_address}. "
-                f"no export of records."
+                f"cache & records don't span more than 1 minute. "
+                f"updating cache. no export of records. cache: {cached}. job: {job}"
             )
             new_cache_wpts: list[SpireWaypointPositional] = []
             if validated_cache:
@@ -140,9 +126,9 @@ def run():
             )
         except Exception:
             logger.error(
-                f"failed to interpolate. "
-                f"batch will not be ack'ed from queue "
-                f"for icao_address: {job.flight_info.icao_address}."
+                f"failed to interpolate."
+                f"not updating cache. not exporting records."
+                f"cache: {cached}. job: {job}"
                 f"traceback: {format_traceback()}"
             )
             sys.exit(1)
@@ -168,17 +154,9 @@ def run():
             records=resampled_records_prune,
         )
 
-        logger.info(
-            f"publishing records to BigQuery pubsub topic:"
-            f" {env.SPIRE_WAYPOINTS_BIGQUERY_TOPIC_ID}."
-        )
         for bq_json_ln in egress_records.to_bq_flatmap():
             bq_publish_handler.publish_async(bq_json_ln)
         bq_publish_handler.wait_for_publish()
-        logger.info(
-            f"published N={len(egress_records.records)} interpolated (imputed) waypoints to "
-            f"{env.SPIRE_WAYPOINTS_BIGQUERY_TOPIC_ID}"
-        )
 
         # TODO: generate flight segments; publish flight segments to pubsub
         # logger.info(
@@ -196,15 +174,12 @@ def run():
             spire_wps=new_cache_records,
         )
         cache_handler.push(new_cache)
-        logger.info(
-            f"updated cache with {len(new_cache.waypoints)} waypoints "
-            f"for icao address {validated_flight_info.icao_address}"
-        )
+        logger.info(f"updated cache: {len(new_cache.waypoints)} waypoints")
 
         logger.info(
             f"finished processing batch "
             f"for icao_address: {egress_records.flight_info.icao_address}. "
-            f"exported {len(egress_records.records)} resampled records "
+            f"exported {len(egress_records.records)} resampled records to BigQuery"
             f"spanning {egress_records.records[0].timestamp} "
             f"to {egress_records.records[-1].timestamp}."
         )
