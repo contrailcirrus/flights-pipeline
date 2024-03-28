62424d1d3879ba5ca3f9ca61249b6cd55b727264
Nick Masson <nick.masson@breakthroughenergy.org>
2024-03-28 12:34:12 -0600

    fix edge case of single pt delivery near cache (#97)
    * handle edge case
    
    * update log msg

--- spire-ingest-resample-worker/scratch.py
+++ spire-ingest-resample-worker/scratch.py
@@ -1,12 +1,12 @@
 from lib.schemas import SpireWaypointsRecord, WaypointCache
-from tests.stubs.stub import ingress_pubsub_bytes, redis_response
+from tests.stubs.stub import ingress_pubsub_bytes, redis_response_3
 from lib.handlers import ValidationHandler, ResampleHandler
 
 
 recs: SpireWaypointsRecord = SpireWaypointsRecord.from_utf8_json(
-    ingress_pubsub_bytes["multi_min_span"]
+    ingress_pubsub_bytes["single_waypoint"]
 )
-cached: list[WaypointCache.Waypoint] = WaypointCache.from_flatmap(redis_response)
+cached: list[WaypointCache.Waypoint] = WaypointCache.from_flatmap(redis_response_3)
 
 validation_handler = ValidationHandler(cached, recs)
 resample_handler = ResampleHandler(
