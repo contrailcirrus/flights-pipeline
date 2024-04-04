7774dc538fb47fec5f3fef63df0ce35c6938c6f1
Ben Fasoli <benfasoli@gmail.com>
2024-04-04 10:50:41 -0700

    api-scraper: use multiple threads to improve Spire throughput (#110)

--- spire-ingest-api-scraper/lib/spire.py
+++ spire-ingest-api-scraper/lib/spire.py
@@ -1,10 +1,13 @@
+import concurrent.futures
 import json
 import time
 from datetime import datetime, timedelta, timezone
+from typing import Any
 
 import pandas as pd
 import requests
 
+from lib import utils
 from lib.log import logger
 
 # Requesting data [start_at, end_at) will fetch [start_at, end_at + INGEST_LAG_TIME]
@@ -87,20 +90,96 @@ class SpireAPIClient:
         if end_at.tzinfo is None:
             raise ValueError("end_at must be timezone aware")
 
-        start_at_utc = start_at.astimezone(timezone.utc)
-        end_at_utc = end_at.astimezone(timezone.utc)
+        start_at = start_at.astimezone(timezone.utc)
+        end_at = end_at.astimezone(timezone.utc)
 
-        end_at_utc_buffer = end_at_utc + INGEST_LAG_TIME
-        if end_at_utc_buffer > datetime.now(timezone.utc):
+        end_at_plus_lag = end_at + INGEST_LAG_TIME
+        if end_at_plus_lag > datetime.now(timezone.utc):
             raise ValueError(
                 f"end_at must be at least {INGEST_LAG_TIME} before present"
             )
 
+        # Decompose job into multiple windows that can be fetched concurrently.
+        window_size = timedelta(minutes=5)
+        windows = utils.time_windows(start_at, end_at_plus_lag, window_size)
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            results = executor.map(
+                lambda window: self._fetch_target_records_with_retry(*window),
+                windows,
+            )
+
+        # Flatten nested list of records returned by each worker thread.
+        target_records = [record for result in results for record in result]
+        df = pd.DataFrame(target_records)
+
+        # Spire API may return icao_address values that are not unique to a specific
+        # aircraft. Drop values known to be duplicated across aircraft.
+        is_ignored_icao_address = df["icao_address"].isin(IGNORE_ICAO_ADDRESS)
+        is_unique_icao_address = ~is_ignored_icao_address
+        df = df.loc[is_unique_icao_address, :]
+
+        drop_count_ignored_icao_address = is_ignored_icao_address.sum()
+        if drop_count_ignored_icao_address > 0:
+            logger.info(
+                f"Drop {drop_count_ignored_icao_address} records with non-unique "
+                + f"icao_address in {IGNORE_ICAO_ADDRESS}"
+            )
+
+        # Spire API's start_at and end_at query params reference ingestion_time
+        # sometimes, the SPIRE API will still return records outside of [start_at, end_at]
+        timestamp = pd.to_datetime(df["timestamp"])
+        ingestion_time = pd.to_datetime(df["ingestion_time"])
+        ingest_at_or_after_start = ingestion_time >= pd.to_datetime(start_at)
+        ingest_before_end_w_buffer = ingestion_time < pd.to_datetime(end_at_plus_lag)
+        df = df.loc[ingest_at_or_after_start & ingest_before_end_w_buffer, :]
+
+        drop_ingest_outside_window = (~ingest_at_or_after_start).sum() + (
+            ~ingest_before_end_w_buffer
+        ).sum()
+        if drop_ingest_outside_window > 0:
+            logger.info(
+                f"Records outside query window. "
+                f"Drop {drop_ingest_outside_window} records."
+            )
+
+        # identify tardy records
+        timestamp = pd.to_datetime(df["timestamp"])
+        ingestion_time = pd.to_datetime(df["ingestion_time"])
+        is_tardy = (ingestion_time - timestamp) > INGEST_LAG_TIME
+        ingest_before_end = ingestion_time < pd.to_datetime(end_at)
+        df_tardy = df.loc[is_tardy & ingest_before_end, :]
+
+        # Drop records with timestamps outside of [start_at, end_at).
+        is_at_or_after_start = timestamp >= pd.to_datetime(start_at)
+        is_before_end = timestamp < pd.to_datetime(end_at)
+        df = df.loc[is_at_or_after_start & is_before_end, :]
+
+        drop_count_before_start = (~is_at_or_after_start).sum()
+        if drop_count_before_start > 0:
+            logger.info(f"Drop {drop_count_before_start} records before {start_at}")
+
+        drop_count_after_end = (~is_before_end).sum()
+        if drop_count_after_end > 0:
+            logger.info(f"Drop {drop_count_after_end} records after {end_at}")
+
+        return df, df_tardy
+
+    def _fetch_target_records_with_retry(
+        self,
+        start_at_utc: datetime,
+        end_at_utc: datetime,
+    ) -> list[dict[str, Any]]:
+        """Sends GET request to Spire API and parses target records from response.
+
+        Failed requests are retried up to 5 times with exponential backoff.
+
+        This method is thread-safe.
+        """
         headers = {"Authorization": f"Bearer {self._api_token}"}
 
         params = {
             "start": start_at_utc.isoformat().replace("+00:00", "Z"),
-            "end": end_at_utc_buffer.isoformat().replace("+00:00", "Z"),
+            "end": end_at_utc.isoformat().replace("+00:00", "Z"),
         }
 
         min_backoff_seconds = 1
@@ -134,56 +213,4 @@ class SpireAPIClient:
             if target_record is not None:
                 target_records.append(target_record)
 
-        df = pd.DataFrame(target_records)
-
-        # Spire API may return icao_address values that are not unique to a specific
-        # aircraft. Drop values known to be duplicated across aircraft.
-        is_ignored_icao_address = df["icao_address"].isin(IGNORE_ICAO_ADDRESS)
-        is_unique_icao_address = ~is_ignored_icao_address
-        df = df.loc[is_unique_icao_address, :]
-
-        drop_count_ignored_icao_address = is_ignored_icao_address.sum()
-        if drop_count_ignored_icao_address > 0:
-            logger.info(
-                f"Drop {drop_count_ignored_icao_address} records with non-unique "
-                + f"icao_address in {IGNORE_ICAO_ADDRESS}"
-            )
-
-        # Spire API's start_at and end_at query params reference ingestion_time
-        # sometimes, the SPIRE API will still return records outside of [start_at, end_at]
-        timestamp = pd.to_datetime(df["timestamp"])
-        ingestion_time = pd.to_datetime(df["ingestion_time"])
-        ingest_at_or_after_start = ingestion_time >= pd.to_datetime(start_at_utc)
-        ingest_before_end_w_buffer = ingestion_time < pd.to_datetime(end_at_utc_buffer)
-        df = df.loc[ingest_at_or_after_start & ingest_before_end_w_buffer, :]
-
-        drop_ingest_outside_window = (~ingest_at_or_after_start).sum() + (
-            ~ingest_before_end_w_buffer
-        ).sum()
-        if drop_ingest_outside_window > 0:
-            logger.info(
-                f"Records outside query window. "
-                f"Drop {drop_ingest_outside_window} records."
-            )
-
-        # identify tardy records
-        timestamp = pd.to_datetime(df["timestamp"])
-        ingestion_time = pd.to_datetime(df["ingestion_time"])
-        is_tardy = (ingestion_time - timestamp) > INGEST_LAG_TIME
-        ingest_before_end = ingestion_time < pd.to_datetime(end_at_utc)
-        df_tardy = df.loc[is_tardy & ingest_before_end, :]
-
-        # Drop records with timestamps outside of [start_at, end_at).
-        is_at_or_after_start = timestamp >= pd.to_datetime(start_at_utc)
-        is_before_end = timestamp < pd.to_datetime(end_at_utc)
-        df = df.loc[is_at_or_after_start & is_before_end, :]
-
-        drop_count_before_start = (~is_at_or_after_start).sum()
-        if drop_count_before_start > 0:
-            logger.info(f"Drop {drop_count_before_start} records before {start_at_utc}")
-
-        drop_count_after_end = (~is_before_end).sum()
-        if drop_count_after_end > 0:
-            logger.info(f"Drop {drop_count_after_end} records after {end_at_utc}")
-
-        return df, df_tardy
+        return target_records
