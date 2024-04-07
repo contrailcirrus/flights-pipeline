8911df11fd7f3b40d57cae6ba94636b5bfbb607b
Nicholas Masson <nick.masson@breakthroughenergy.org>
2024-04-06 18:29:13 -0600

    scaffold

---
+++ trajectory-worker/README.md
@@ -0,0 +1,19 @@
+# Trajectory Worker
+
+## Overview
+A Kubernetes deployment that ingests a set of contiguous waypoints ("flight trajectory chunk")
+for a given flight-instance, and runs CoCip on that trajectory segment.
+The set of waypoints in each job contains one additional leading waypoint 
+(and by extension, one additional leading segment), which serves as a sacrificial segment for 
+running CoCip. 
+
+# Behavior
+
+
+## Environment Variables
+The following environment variables are expected for production and development environments.
+
+| name                             |                                description                                |
+|:---------------------------------|:-------------------------------------------------------------------------:|
+| TRAJECTORY_CHUNK_SUBSCRIPTION_ID | fully-qualified uri for the flights trajectory chunks pubsub subscription |
+| LOG_LEVEL                        |                log level for service in cloud environment                 |
---
+++ trajectory-worker/main.py
@@ -0,0 +1,60 @@
+"""Entrypoint for the Spire Ingest Resample Worker."""
+
+import sys
+import time
+
+import lib.environment as env
+from lib import utils
+from lib.handlers import (
+    PubSubSubscriptionHandler,
+)
+from lib.log import format_traceback, logger
+from lib.schemas import (
+    SpireWaypointsRecord,
+)
+
+
+def run():
+    """
+    Main entrypoint.
+    - Dequeue a set of waypoints (trajectory chunk)
+    - Run cocip against trajectory
+    - Export values (big query, other TBD)
+    """
+
+    with PubSubSubscriptionHandler(env.TRAJECTORY_CHUNK_SUBSCRIPTION_ID) as job_handler:
+        # ===================
+        # fetch records
+        # ===================
+        job: SpireWaypointsRecord = job_handler.fetch()
+        if not job:
+            # if the queue is empty -> we get back [], then pause before retry
+            logger.info("job empty. sleeping... ")
+            time.sleep(10)
+            return
+
+        logger.info(
+            f"got job with {len(job.records)} records. "
+            f"icao_address: {job.flight_info.icao_address}. "
+            f"spanning: {job.records[0].timestamp} to {job.records[-1].timestamp}"
+        )
+
+        # ===================
+        # apply CoCip Trajectory model
+        # ===================
+        # TODO
+        time.sleep(500)
+        job_handler.ack()
+
+
+if __name__ == "__main__":
+    logger.info("starting trajectory-worker instance")
+    sigterm_handler = utils.SigtermHandler()
+    while True:
+        if sigterm_handler.should_exit:
+            sys.exit(0)
+        try:
+            run()
+        except Exception:
+            logger.error("Unhandled exception:" + format_traceback())
+            sys.exit(1)
