30c9a8ff54468667b865045c29f1115dc6af4181
Ben Fasoli <benfasoli@gmail.com>
2024-04-09 11:54:37 -0700

    Install custom metrics adapter for HPA external metrics

--- .cloud/auth.tf
+++ .cloud/auth.tf
@@ -1,3 +1,20 @@
+data "google_compute_default_service_account" "default" {}
+
+resource "google_project_iam_member" "default_monitoring_viewer_binding" {
+  member  = data.google_compute_default_service_account.default.member
+  project = "contrails-301217"
+  role    = "roles/monitoring.viewer" # Required for GKE custom metrics adapter
+}
+
+resource "google_service_account_iam_binding" "default_iam_workload_identity_user_binding" {
+  service_account_id = data.google_compute_default_service_account.default.name
+  role               = "roles/iam.workloadIdentityUser" # Required for GKE custom metrics adapter
+  members = [
+    # See: https://github.com/GoogleCloudPlatform/k8s-stackdriver/blob/master/custom-metrics-stackdriver-adapter
+    "serviceAccount:contrails-301217.svc.id.goog[custom-metrics/custom-metrics-stackdriver-adapter]",
+  ]
+}
+
 resource "google_service_account" "flights_pipeline_sa" {
   account_id                   = "flights-pipeline"
   create_ignore_already_exists = null
@@ -40,7 +57,6 @@ resource "google_project_iam_member" "flights_pipeline_sa_binding" {
   ]
 }
 
-
 resource "google_service_account_iam_binding" "k8s_sa_to_flights_pipeline_sa_binding" {
   service_account_id = google_service_account.flights_pipeline_sa.id
   role               = "roles/iam.workloadIdentityUser"
@@ -61,25 +77,25 @@ resource "google_service_account_iam_binding" "k8s_sa_to_flights_pipeline_sa_bin
 # these roles are required for the pubsub -> bigquery integration
 resource "google_project_iam_member" "pubsub_to_bigquery_sa_binding_viever" {
   project = "contrails-301217"
-  role   = "roles/bigquery.metadataViewer"
-  member = "serviceAccount:service-577335432373@gcp-sa-pubsub.iam.gserviceaccount.com"
+  role    = "roles/bigquery.metadataViewer"
+  member  = "serviceAccount:service-577335432373@gcp-sa-pubsub.iam.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "pubsub_to_bigquery_sa_binding_editor" {
   project = "contrails-301217"
-  role   = "roles/bigquery.dataEditor"
-  member = "serviceAccount:service-577335432373@gcp-sa-pubsub.iam.gserviceaccount.com"
+  role    = "roles/bigquery.dataEditor"
+  member  = "serviceAccount:service-577335432373@gcp-sa-pubsub.iam.gserviceaccount.com"
 }
 
 # these roles are required for subscribers to publish failed msgs to a dead letter topic
 resource "google_project_iam_member" "pubsub_to_dead_letter_sa_binding_publisher" {
   project = "contrails-301217"
-  role   = "roles/pubsub.publisher"
-  member = "serviceAccount:service-577335432373@gcp-sa-pubsub.iam.gserviceaccount.com"
+  role    = "roles/pubsub.publisher"
+  member  = "serviceAccount:service-577335432373@gcp-sa-pubsub.iam.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "pubsub_to_dead_letter_sa_binding_subscriber" {
   project = "contrails-301217"
-  role   = "roles/pubsub.subscriber"
-  member = "serviceAccount:service-577335432373@gcp-sa-pubsub.iam.gserviceaccount.com"
+  role    = "roles/pubsub.subscriber"
+  member  = "serviceAccount:service-577335432373@gcp-sa-pubsub.iam.gserviceaccount.com"
 }
