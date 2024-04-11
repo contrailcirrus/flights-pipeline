70ebe7fa5d9b3b0e81ca4f19927175668cde3b96
Ben Fasoli <benfasoli@gmail.com>
2024-04-11 07:13:32 -0700

    Add Spire concurrency control (#121)
    * Reduce size of Spire test data fixture
    
    * Add Spire concurrency control

--- spire-ingest-api-scraper/tests/conftest.py
+++ spire-ingest-api-scraper/tests/conftest.py
@@ -23,11 +23,11 @@ def _read_text(filename: str) -> str:
         return f.read()
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def mock_spire_airsafe_api() -> Iterator[str]:
     """Return cached response json from given URL during testing."""
     mock_url = "https://api-mock.airsafe.spire.com/v2/targets/stream"
-    mock_body = _read_text("tests/api/spire_response_1min.ndjson")
+    mock_body = _read_text("tests/api/spire_response_10k.ndjson")
     with responses.RequestsMock() as mock_responses:
         mock_responses.get(
             url=mock_url,
