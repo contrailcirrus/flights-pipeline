91e13b35831c2269f1e9489e19b8aea14abe1bac
Ben Fasoli <benfasoli@gmail.com>
2024-04-18 16:46:18 -0700

    Limit PubSub publish batch size to 1,000 messages (#137)

--- spire-ingest-api-scraper/lib/queue.py
+++ spire-ingest-api-scraper/lib/queue.py
@@ -47,7 +47,7 @@ class QueueClient:
             # Batch settings increase payload size to execute fewer, larger requests.
             # See: https://cloud.google.com/pubsub/docs/batch-messaging
             batch_settings=pubsub_v1.types.BatchSettings(
-                max_messages=2500,
+                max_messages=1000,
                 max_bytes=20 * 1000 * 1000,  # 20 MB max server-side request size
                 max_latency=5,  # default: 10 ms
             ),
