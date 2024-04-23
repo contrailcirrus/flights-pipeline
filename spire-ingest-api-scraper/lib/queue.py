1e04fa4f487bdf5c07a51e959418acbe80a95aab
Ben Fasoli <benfasoli@gmail.com>
2024-04-23 16:20:03 -0700

    Set api-scraper batch latency to 1 second

--- spire-ingest-api-scraper/lib/queue.py
+++ spire-ingest-api-scraper/lib/queue.py
@@ -49,7 +49,7 @@ class QueueClient:
             batch_settings=pubsub_v1.types.BatchSettings(
                 max_messages=1000,
                 max_bytes=20 * 1000 * 1000,  # 20 MB max server-side request size
-                max_latency=5,  # default: 10 ms
+                max_latency=1,  # default: 10 ms
             ),
             publisher_options=pubsub_v1.types.PublisherOptions(
                 enable_message_ordering=ordered_queue,
