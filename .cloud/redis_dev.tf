2f563ca12924f98a622cc3d18531ffa263361981
Nicholas Masson <nick.masson@breakthroughenergy.org>
2024-04-23 15:40:43 -0600

    move redis location; update host

--- .cloud/redis_dev.tf
+++ .cloud/redis_dev.tf
@@ -3,8 +3,8 @@ resource "google_redis_instance" "flights_pipeline_redis_cache_dev" {
   tier           = "STANDARD_HA"
   memory_size_gb = 1
 
-  location_id             = "us-central1-c"
-  alternative_location_id = "us-central1-a"
+  location_id             = "us-east1-c"
+  alternative_location_id = "us-east1-b"
 
   redis_version     = "REDIS_7_0"
   display_name      = "flight-instance-dev"
