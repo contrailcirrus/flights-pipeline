97d159d575cb1016beff552a3b4ba78da9944871
Nicholas Masson <nick.masson@breakthroughenergy.org>
2024-05-02 15:47:39 -0600

    add helper; remove bada4 from download

---
+++ trajectory-worker/bada_lookup.py
@@ -0,0 +1,28 @@
+"""
+A script to verify if a target aircraft icao code is recognized by the BADA3 dataset.
+Retrieves the default/defined engine specs for the specified aircraft.
+"""
+
+from pycontrails_bada.bada3 import BADA3
+import json
+
+# gcloud storage cp --quiet --recursive --no-clobber gs://contrails-301217-bada/bada/bada3 .
+BADA3_FP = "bada3"
+
+bada = BADA3(bada_path=BADA3_FP)
+
+if __name__ == "__main__":
+    aircraft_type_icao = "E75L"  # in bada3
+    # aircraft_type_icao = 'EC20'  # not in bada3
+
+if bada.check_aircraft_type_availability(aircraft_type_icao, raise_error=False):
+    print(f"{aircraft_type_icao} found in BADA")
+
+    print("fetching engine specs...")
+    engine_props = bada.get_aircraft_engine_properties(
+        atyp_icao=aircraft_type_icao,
+    )
+    print(json.dumps(engine_props.__dict__, indent=4))
+
+else:
+    print(f"{aircraft_type_icao} NOT found in BADA")
