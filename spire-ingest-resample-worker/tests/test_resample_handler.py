f10b536781a45481a512d4ba3d6c20a9d1b83f40
Ben Fasoli <benfasoli@gmail.com>
2024-05-14 11:01:42 -0700

    Fix resample-worker tests (#224)

--- spire-ingest-resample-worker/tests/test_resample_handler.py
+++ spire-ingest-resample-worker/tests/test_resample_handler.py
@@ -4,37 +4,44 @@ import lib.handlers
 from lib.schemas import SpireWaypointPositional, SpireWaypointsRecord
 from tests.stubs.stub import ingress_pubsub_bytes
 
-recs_multi_min_span: list[SpireWaypointPositional] = (
-    SpireWaypointsRecord.from_utf8_json(ingress_pubsub_bytes["multi_min_span"]).records
-)
-recs_gt_one_min: list[SpireWaypointPositional] = SpireWaypointsRecord.from_utf8_json(
+recs_multi_min_span = SpireWaypointsRecord.from_utf8_json(
+    ingress_pubsub_bytes["multi_min_span"]
+).records
+recs_gt_one_min = SpireWaypointsRecord.from_utf8_json(
     ingress_pubsub_bytes["gt_one_min_span"]
 ).records
-recs_lt_inter_one_min: list[SpireWaypointPositional] = (
-    SpireWaypointsRecord.from_utf8_json(
-        ingress_pubsub_bytes["lt_inter_one_min_span"]
-    ).records
-)
-recs_lt_intra_one_min: list[SpireWaypointPositional] = (
-    SpireWaypointsRecord.from_utf8_json(
-        ingress_pubsub_bytes["lt_intra_one_min_span"]
-    ).records
-)
+recs_lt_inter_one_min = SpireWaypointsRecord.from_utf8_json(
+    ingress_pubsub_bytes["lt_inter_one_min_span"]
+).records
+recs_lt_intra_one_min = SpireWaypointsRecord.from_utf8_json(
+    ingress_pubsub_bytes["lt_intra_one_min_span"]
+).records
 
 
-@pytest.mark.parametrize("recs", [recs_lt_intra_one_min, recs_lt_inter_one_min])
-def test_interpolate_lt_one_min_span(recs: list[SpireWaypointPositional]):
+def test_interpolate_points_within_single_minute_returns_no_waypoints():
     """
     case: no cache, multiple records,
-     1. records span less that 1min, crosses 1min timestamp
-     2. records span less than 1min, does not cross 1min timestamp
+     1. records span less than 1min, does not cross 1min timestamp
     """
     h = lib.handlers.ResampleHandler(
         cache=[],
-        records_window=recs,
+        records_window=recs_lt_intra_one_min,
     )
     interpolated_waypoints = h.interpolate().waypoints_resampled
     assert len(interpolated_waypoints) == 0
+
+
+def test_interpolate_points_lt_one_min_spanning_next_minute_returns_one_waypoint():
+    """
+    case: no cache, multiple records,
+     1. records span less that 1min, crosses 1min timestamp
+    """
+    h = lib.handlers.ResampleHandler(
+        cache=[],
+        records_window=recs_lt_inter_one_min,
+    )
+    interpolated_waypoints = h.interpolate().waypoints_resampled
+    assert len(interpolated_waypoints) == 1
 
 
 @pytest.mark.parametrize("recs", [recs_gt_one_min, recs_gt_one_min])
