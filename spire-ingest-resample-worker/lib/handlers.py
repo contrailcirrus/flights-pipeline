dbdc9380b21bdca0d44e46f8708a024468c9d9f4
Ben Fasoli <benfasoli@gmail.com>
2024-05-20 19:09:43 -0700

    Retry DeadlineExceeded errors

--- spire-ingest-resample-worker/lib/handlers.py
+++ spire-ingest-resample-worker/lib/handlers.py
@@ -77,9 +77,25 @@ class PubSubSubscriptionHandler:
         """
         while True:
             logger.info(f"fetching message from {self.subscription}")
+
             resp = self._client.pull(
                 request={"subscription": self.subscription, "max_messages": 1},
-                timeout=self.pull_timeout_sec,
+                timeout=self.pull_timeout_sec,  # default: 60
+                retry=google.api_core.retry.Retry(
+                    initial=0.1,  # default: 0.1
+                    maximum=60.0,  # default: 60
+                    multiplier=1.3,  # default: 1.3
+                    predicate=google.api_core.retry.if_exception_type(
+                        # Non-default exceptions:
+                        google.api_core.exceptions.DeadlineExceeded,
+                        # Default exceptions:
+                        google.api_core.exceptions.Aborted,
+                        google.api_core.exceptions.InternalServerError,
+                        google.api_core.exceptions.ServiceUnavailable,
+                        google.api_core.exceptions.Unknown,
+                    ),
+                    deadline=60.0,  # default: 60
+                ),
             )
 
             if len(resp.received_messages) == 0:
