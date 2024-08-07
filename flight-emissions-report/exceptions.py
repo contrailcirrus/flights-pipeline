02388d248730841a27fb28ef092b6c62e9b85dcb
Nicholas Masson <nick.masson@breakthroughenergy.org>
2024-08-07 09:13:13 -0600

    add custom exceptions

---
+++ flight-emissions-report/exceptions.py
@@ -0,0 +1,33 @@
+"""
+Custom exceptions.
+"""
+
+
+class OriginDestinationError(Exception):
+    """
+    Trajectory is not originating or terminating at expected location.
+    """
+
+
+class FlightTooShortError(Exception):
+    """
+    Trajectory is unreasonably short in flight time.
+    """
+
+
+class FlightTooLongError(Exception):
+    """
+    Trajectory is unreasonably long in flight time.
+    """
+
+
+class FlightTooSlowError(Exception):
+    """
+    Trajectory has period(s) of unrealistically slow speed.
+    """
+
+
+class FlightTooFastError(Exception):
+    """
+    Trajectory has period(s) of unrealistically high speed.
+    """
