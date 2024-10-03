485b35202947472e94ef6b2a1473748e1e61982e
Nicholas Masson <nick.masson@breakthroughenergy.org>
2024-10-03 09:03:51 -0600

    add in_conus aggs to report fetch query

--- flight-emissions-report/sql/bq_flights_report_daily.sql
+++ flight-emissions-report/sql/bq_flights_report_daily.sql
@@ -13,7 +13,9 @@ WITH base_tb AS (SELECT *
          (SELECT *,
                  ((time_start_sunrise_offset_mins <= 0) AND (time_start_sunset_offset_mins <= 3 * 60)) OR
                  ((0 < time_start_sunrise_offset_mins) AND (time_start_sunset_offset_mins < 0)) OR
-                 ((-3 * 60 <= time_start_sunrise_offset_mins) AND (0 <= time_start_sunset_offset_mins)) AS is_nighttime
+                 ((-3 * 60 <= time_start_sunrise_offset_mins) AND (0 <= time_start_sunset_offset_mins)) AS is_nighttime,
+                 ST_INTERSECTS(ST_GEOGPOINT(lon_start, lat_start),
+                               ST_GEOGFROMTEXT("POLYGON((-134.03 50.07, -121.2 14.9, -63.2 10.5, -46.1 44.1, -134.03 50.07))")) AS in_conus,
           FROM base_tb
           WHERE seg_cnt = 1),
      ranked_candidate_flights_tb AS
@@ -54,7 +56,41 @@ WITH base_tb AS (SELECT *
                                             contrail_dist_km         AS daytime_contrail_dist_km,
                                             warming_contrail_dist_km AS daytime_warming_contrail_dist_km,
                                      FROM nighttime_agg_tb
-                                     WHERE is_nighttime IS FALSE) dt_tb ON nt_tb.flight_id = dt_tb.flight_id)
+                                     WHERE is_nighttime IS FALSE) dt_tb ON nt_tb.flight_id = dt_tb.flight_id),
+     in_conus_agg_tb AS (SELECT flight_id,
+                                in_conus,
+                                SUM(chunk_len_km)                               AS dist_km,
+                                SUM(sum_ef_mj)                                  AS sum_ef_mj,
+                                SUM(total_persistent_contrail_length_km)        AS contrail_dist_km,
+                                SUM(total_pos_ef_persistent_contrail_length_km) AS warming_contrail_dist_km
+                         FROM candidate_segments_tb
+                         GROUP BY flight_id, in_conus
+                         ORDER BY flight_id),
+     flat_in_conus_agg_tb AS (SELECT COALESCE(in_tb.flight_id, out_tb.flight_id) AS flight_id,
+                                     in_conus_dist_km,
+                                     in_conus_sum_ef_mj,
+                                     in_conus_contrail_dist_km,
+                                     in_conus_warming_contrail_dist_km,
+                                     out_conus_dist_km,
+                                     out_conus_sum_ef_mj,
+                                     out_conus_contrail_dist_km,
+                                     out_conus_warming_contrail_dist_km,
+                              FROM (SELECT flight_id,
+                                           dist_km                  AS in_conus_dist_km,
+                                           sum_ef_mj                AS in_conus_sum_ef_mj,
+                                           contrail_dist_km         AS in_conus_contrail_dist_km,
+                                           warming_contrail_dist_km AS in_conus_warming_contrail_dist_km
+                                    FROM in_conus_agg_tb
+                                    WHERE in_conus IS TRUE) in_tb
+                                       FULL OUTER JOIN
+                                   (SELECT flight_id,
+                                           dist_km                  AS out_conus_dist_km,
+                                           sum_ef_mj                AS out_conus_sum_ef_mj,
+                                           contrail_dist_km         AS out_conus_contrail_dist_km,
+                                           warming_contrail_dist_km AS out_conus_warming_contrail_dist_km
+                                    FROM in_conus_agg_tb
+                                    WHERE in_conus IS FALSE) out_tb ON in_tb.flight_id = out_tb.flight_id)
 SELECT *
 FROM summary_flights_tb sf_tb
          LEFT JOIN flat_nighttime_agg_tb fna_tb ON sf_tb.flight_id = fna_tb.flight_id
+         LEFT JOIN flat_in_conus_agg_tb fica_tb ON sf_tb.flight_id = fica_tb.flight_id
