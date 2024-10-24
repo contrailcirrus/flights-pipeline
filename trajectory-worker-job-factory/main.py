4e822d4fbced5a12df4aeea8762841af64f40c14
Nicholas Masson <nick.masson@breakthroughenergy.org>
2024-10-24 10:19:59 -0600

    patch; job factory, fix egress queue in env vars; update logging to not print json literal

--- trajectory-worker-job-factory/main.py
+++ trajectory-worker-job-factory/main.py
@@ -38,7 +38,9 @@ def run(
         job_hash = hashlib.shake_128(job.as_utf8_json()).hexdigest(
             8
         )  # useful for keying in logs
-        logger.info(f"got TJWD {job_hash}: {job.as_utf8_json()}")
+        logger.info(
+            f"got TJWD {job_hash}: {[f'{k}:{v} ' for k, v in job.as_utf8_json().items()]}"
+        )
 
         try:
             job_builder_svc.run(twjd=job)
