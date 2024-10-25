717f57994726f882fb2b7a48bd00d419853cdda6
Nicholas Masson <nick.masson@breakthroughenergy.org>
2024-10-25 11:22:00 -0600

    wire in bq handler to entrypoint

--- flight-emissions-report-cache/main.py
+++ flight-emissions-report-cache/main.py
@@ -3,16 +3,21 @@ import sys
 from datetime import datetime, UTC, timedelta
 from typing import TypedDict
 
+import pandas as pd
 import pg8000
 import sqlalchemy
+from google.cloud import bigquery
 from google.cloud.sql.connector import Connector, IPTypes
 
 import lib.environment as env
 from lib.log import logger
+from lib.handlers import BigQueryHandler
 
 SYNC_OFFSET_DAYS = (
     2  # lag, in days, between now() and the target day for sync'ing records
 )
+
+BQ_FER_AIRLINES_DAY_QUERY_FILENAME = "sql/bq_fer_by_airlines_day.sql"
 
 
 class ResponseObject(TypedDict):
@@ -142,13 +147,6 @@ class MyBlob:
     """
 
 
-def bq_fetch(query_str: str) -> MyBlob:
-    """
-    Given a valid BQ query string, execute the query, and return the records.
-    """
-    return MyBlob()
-
-
 def psdb_push(records: MyBlob):
     """
     Given a batch of records, push those records to a postgres database table.
@@ -170,12 +168,28 @@ if __name__ == "__main__":
         "%Y-%m-%d"
     )  # target date str as submitted to BQ
 
+    bq_handler = BigQueryHandler()
+
     try:
-        # TODO - fetch target records from BQ
-        records: MyBlob = bq_fetch("query_string")
+        query = bq_handler.import_query(BQ_FER_AIRLINES_DAY_QUERY_FILENAME)
+        cfg = bigquery.QueryJobConfig(
+            query_parameters=[
+                bigquery.ArrayQueryParameter(
+                    "airline_iata_lst",
+                    "STRING",
+                    [v for k, v in TARGET_AIRLINES.items()],
+                ),
+                bigquery.ScalarQueryParameter(
+                    "date_str",
+                    "STRING",
+                    "2024-09-01",
+                ),
+            ]
+        )
+        records_df: pd.DataFrame = bq_handler.query(query, cfg)
 
         # TODO - push records to psdb
-        psdb_push(records)
+        psdb_push(records_df)
     except Exception as e:
         logger.error(
             f"failed to sync {target_date_str} between BQ table and PSDB table. {e}"
