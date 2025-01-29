008b4284fcca63a92d6863936b2210a8458fb0b3
Nicholas Masson <nick.masson@breakthroughenergy.org>
2025-01-29 14:40:07 -0700

    update bq schema; update cron to new ETL data models

--- nat-track-cacher/main.py
+++ nat-track-cacher/main.py
@@ -17,7 +17,6 @@ if __name__ == "__main__":
         # fetch geoJSON blob from API
         resp = requests.get(env.NAT_TRACK_API_URL)
         geojson_blob = resp.json()
-        updated_at_iso_str = geojson_blob["features"][0]["properties"]["updated_at"]
 
         # write features to BigQuery
         bigquery_client = bigquery.Client()
@@ -26,19 +25,39 @@ if __name__ == "__main__":
         for feature in geojson_blob["features"]:
             geo_str = json.dumps(feature["geometry"])
             props = feature["properties"]
-            insert_rows.append(
-                {
-                    "waypoint_id": props.get("id"),
-                    "updated_at": updated_at_iso_str,
-                    "nat": geo_str,
-                }
-            )
+            geo_type = feature["geometry"]["type"]
+
+            if geo_type == "LineString":
+                # this is the full NAT track geo object
+                insert_rows.append(
+                    {
+                        "nat_identifier": props["name"],
+                        "updated_at": props["updated_at"],
+                        "nat": geo_str,
+                        "flight_levels_west": props["flight_levels_west"],
+                        "flight_levels_east": props["flight_levels_east"],
+                        "valid_dates": props["valid_dates"],
+                    }
+                )
+            elif geo_type == "Point":
+                # this is a waypoint in a NAT track geo object
+                insert_rows.append(
+                    {
+                        "nat_identifier": props["id"],
+                        "updated_at": props["updated_at"],
+                        "nat": geo_str,
+                        "flight_levels_west": [],
+                        "flight_levels_east": [],
+                        "valid_dates": [],
+                    }
+                )
+            else:
+                raise ValueError(f"found unrecognized geojson Feature type: {type}")
+
         err = bigquery_client.insert_rows_json(env.BQ_TABLE_ID, insert_rows)
         if err:
             raise RuntimeError(f"row insert failed: {err}")
-        logger.info(
-            f"wrote {len(insert_rows)} records to BQ for NATs updated on {updated_at_iso_str}"
-        )
+        logger.info(f"wrote {len(insert_rows)} records to BQ for NATs.")
     except Exception:
         logger.error("Unhandled exception:" + format_traceback())
         sys.exit(1)
