55ef04693f55619e72e5e92af81f6387dd497822
Nicholas Masson <nick.masson@breakthroughenergy.org>
2025-04-30 10:58:42 -0600

    fix; cast env var as int on import

--- trajectory-worker/lib/environment.py
+++ trajectory-worker/lib/environment.py
@@ -13,7 +13,7 @@ HRES_SOURCE_PATH = os.environ["HRES_SOURCE_PATH"]
 ERA5_SOURCE_PATH = os.environ["ERA5_SOURCE_PATH"]
 TRAJECTORY_COCIP_BQ_TOPIC_ID = os.environ["TRAJECTORY_COCIP_BQ_TOPIC_ID"]
 GCP_SVC_ACCT_KEY = json.loads(os.environ["GCP_SVC_ACCT_KEY"])
-N_JOBS = os.environ["N_JOBS"]
+N_JOBS = int(os.environ["N_JOBS"])
 
 LOG_LEVEL = os.environ.get("LOG_LEVEL", "DEBUG")
 GIT_SHA = os.environ.get("GIT_SHA", "local")
