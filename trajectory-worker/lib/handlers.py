dff9d92c4549efbab4bf68c56a0dd4336997ee8d
Nicholas Masson <nick.masson@breakthroughenergy.org>
2025-05-01 11:50:45 -0600

    fix; add default params to aircraft perf wrapper class

--- trajectory-worker/lib/handlers.py
+++ trajectory-worker/lib/handlers.py
@@ -18,7 +18,10 @@ import pandas as pd  # type: ignore
 import xarray as xr
 from google.cloud import pubsub_v1  # type: ignore
 from pycontrails import Flight, MetDataset
-from pycontrails.core.aircraft_performance import AircraftPerformance
+from pycontrails.core.aircraft_performance import (
+    AircraftPerformance,
+    AircraftPerformanceParams,
+)
 from pycontrails.models.cocip import Cocip
 from pycontrails.models.humidity_scaling import (
     ExponentialBoostLatitudeCorrectionHumidityScaling,
@@ -388,6 +391,7 @@ class TrajectoryWorkerAP(AircraftPerformance):
 
     name = "trajectory_worker_ap"
     long_name = "Trajectory Worker Aircraft Performance"
+    default_params = AircraftPerformanceParams
 
     PERF_MODEL_LOOKUP_FP = "lib/perf_model_aircraft_lookup_041824.json"
     BADA3_DATASET_FP = "bada3"
