9ba0d6dd386bb026c78d03eb613560956dfbf72f
Nick Masson <nick.masson@breakthroughenergy.org>
2025-05-21 08:37:45 -0600

    Revert/traj worker job batching (#354)
    * randomize ordering; ie no ordering
    
    * add note about unused env var
    
    * note unused env var
    
    * revert main entrypoint & DTOs
    
    * refactor cocip handler init and met load
    
    * retain new method for fetching met/rad variables and standardizing metdataset
    
    * refactor cocip handler run entrypoint
    
    * fix; and use aircraft perf wrapper

--- trajectory-worker/main.py
+++ trajectory-worker/main.py
@@ -4,7 +4,7 @@ import sys
 
 import lib.environment as env
 from lib import schemas
-from lib.schemas import WaypointsRecord
+from lib.exceptions import FlightTooLowError, AircraftTypeUnrecognizedError
 from lib.utils import sigterm_manager
 from lib.handlers import (
     CocipTrajectoryHandler,
@@ -25,34 +25,49 @@ def run(
     - Run cocip against trajectory
     - Export values (big query, other TBD)
     """
-    for messages in job_handler.subscribe():
+    for message in job_handler.subscribe():
         if sigterm_manager.should_exit:
             sys.exit(0)
+
+        job = schemas.WaypointsRecord.from_utf8_json(message.data)
+
+        logger.info(
+            f"airline_iata: {job.flight_info.airline_iata}"
+            f"flight_id: {job.flight_info.flight_id}. "
+            f"got job with {len(job.records)} records."
+        )
 
         # ===================
         # apply CoCip Trajectory model
         # ===================
-        trajectory_cocip_handler = CocipTrajectoryHandler(
-            messages, env.HRES_SOURCE_PATH, env.ERA5_SOURCE_PATH
-        )
-
-        # remove messages from queue that we know cannot be processed
-        if trajectory_cocip_handler.unprocessable_messages:
-            job_handler.ack(trajectory_cocip_handler.unprocessable_messages)
-
-        if len(trajectory_cocip_handler.all_jobs) == 0:
-            logger.warning("no flights to process. proceeding to next batch...")
+        try:
+            trajectory_cocip_handler = CocipTrajectoryHandler(
+                job, env.HRES_SOURCE_PATH, env.ERA5_SOURCE_PATH
+            )
+        except (FlightTooLowError, AircraftTypeUnrecognizedError) as e:
+            logger.warning(
+                f"airline_iata: {job.flight_info.airline_iata}. "
+                f"skipping {job.flight_info.flight_id}. "
+                f"aircraft_type_icao: {job.flight_info.aircraft_type_icao}. "
+                f"could not run cocip. "
+                f"{e}"
+            )
+            job_handler.ack(message)
             continue
 
         try:
             trajectory_cocip_handler.load()
-            trajectory_cocip_handler.run()
+            cocip_result = trajectory_cocip_handler.run()
         except Exception:
             logger.error(
-                f"NACK'ing (pubsub retry)." f"cocip failed. " f"{format_traceback()}"
+                f"NACK'ing (pubsub retry)."
+                f"airline_iata: {job.flight_info.airline_iata}. "
+                f"flight_id: {job.flight_info.flight_id}. "
+                f"aircraft_type_icao: {job.flight_info.aircraft_type_icao}. "
+                f"cocip failed. "
+                f"{format_traceback()}"
             )
-            for msg in messages:
-                job_handler.nack(msg)
+            job_handler.nack(message)
             continue
 
         now = datetime.now(tz=UTC)
@@ -62,66 +77,59 @@ def run(
         # ===================
         logger.debug("publishing cocip outputs to BQ.")
 
-        job: WaypointsRecord
-        for job in trajectory_cocip_handler.all_jobs:
-            fq_zarr_uri: str = ""
-            # qualify the zarr uri with the source type
-            if job.met_source == schemas.MetSource.HRES:
-                fq_zarr_uri = (
-                    f"HRES/{'-'.join(trajectory_cocip_handler.hres_zarr_uris)}"
-                )
-            elif job.met_source == schemas.MetSource.ERA5:
-                fq_zarr_uri = (
-                    f"ERA5/{'-'.join(trajectory_cocip_handler.era5_zarr_uris)}"
-                )
+        fq_zarr_uri: str
+        # qualify the zarr uri with the source type
+        if job.met_source == schemas.MetSource.HRES:
+            fq_zarr_uri = f"HRES/{trajectory_cocip_handler.zarr_uri}"
+        elif job.met_source == schemas.MetSource.ERA5:
+            fq_zarr_uri = f"ERA5/{'-'.join(trajectory_cocip_handler.zarr_uri)}"
+        else:
+            raise ValueError("traj worker job met source not recognized")
 
-            output = schemas.CocipTrajectoryChunk.from_cocip_result(
-                source_id=job.pubsub_message.ordering_key.split(":")[0],
+        output = schemas.CocipTrajectoryChunk.from_cocip_result(
+            source_id=message.ordering_key.split(":")[0],
+            git_sha=env.GIT_SHA,
+            input_chunk=job,
+            zarr_uri=fq_zarr_uri,
+            result=cocip_result,
+        )
+
+        trajectory_cocip_bq_publisher.publish_async(
+            data=output.to_bq_flatmap(processed_at=now),
+            timeout_seconds=110,
+            log_context=dict(
+                client_name="trajectory_cocip_bq_publisher_traj_summary",
+                icao_address=output.icao_address,
+                source_id=output.source_id,
+                time_start=output.time_start,
+            ),
+        )
+        trajectory_cocip_bq_publisher.wait_for_publish(timeout_seconds=120)
+        # ===================
+        # if enabled, publish all trajectory segments to BQ
+        # ===================
+        if job.export_cocip_trajectory:
+            logger.debug("exporting per-segment cocip outputs to BQ.")
+            seg_outputs = schemas.CocipTrajectoryChunk.from_cocip_result_all_segs(
+                source_id=message.ordering_key.split(":")[0],
                 git_sha=env.GIT_SHA,
                 input_chunk=job,
                 zarr_uri=fq_zarr_uri,
-                result=job.pycontrail_cocip_result,
+                result=cocip_result,
             )
-
-            trajectory_cocip_bq_publisher.publish_async(
-                data=output.to_bq_flatmap(processed_at=now),
-                timeout_seconds=110,
-                log_context=dict(
-                    client_name="trajectory_cocip_bq_publisher_traj_summary",
-                    icao_address=output.icao_address,
-                    source_id=output.source_id,
-                    time_start=output.time_start,
-                ),
-            )
-
-            # ===================
-            # if enabled, publish all trajectory segments to BQ
-            # ===================
-            if job.export_cocip_trajectory:
-                logger.debug("exporting per-segment cocip outputs to BQ.")
-                seg_outputs = schemas.CocipTrajectoryChunk.from_cocip_result_all_segs(
-                    source_id=job.pubsub_message.ordering_key.split(":")[0],
-                    git_sha=env.GIT_SHA,
-                    input_chunk=job,
-                    zarr_uri=fq_zarr_uri,
-                    result=job.pycontrail_cocip_result,
+            for seg in seg_outputs:
+                trajectory_cocip_bq_publisher.publish_async(
+                    data=seg.to_bq_flatmap(processed_at=now),
+                    timeout_seconds=110,
+                    log_context=dict(
+                        client_name="trajectory_cocip_bq_publisher_traj_per_seg",
+                        icao_address=output.icao_address,
+                        source_id=output.source_id,
+                        time_start=output.time_start,
+                    ),
                 )
-                for seg in seg_outputs:
-                    trajectory_cocip_bq_publisher.publish_async(
-                        data=seg.to_bq_flatmap(processed_at=now),
-                        timeout_seconds=110,
-                        log_context=dict(
-                            client_name="trajectory_cocip_bq_publisher_traj_per_seg",
-                            icao_address=output.icao_address,
-                            source_id=output.source_id,
-                            time_start=output.time_start,
-                        ),
-                    )
         trajectory_cocip_bq_publisher.wait_for_publish(timeout_seconds=120)
-
-        job_handler.ack(
-            [job.pubsub_message for job in trajectory_cocip_handler.all_jobs]
-        )
+        job_handler.ack(message)
 
 
 if __name__ == "__main__":
@@ -132,11 +140,7 @@ if __name__ == "__main__":
             topic_id=env.TRAJECTORY_COCIP_BQ_TOPIC_ID,
             ordered_queue=False,
         )
-        job_handler = PubSubSubscriptionHandler(
-            env.TRAJECTORY_CHUNK_SUBSCRIPTION_ID,
-            max_msgs=env.N_JOBS,
-            ack_extension_sec=30,
-        )
+        job_handler = PubSubSubscriptionHandler(env.TRAJECTORY_CHUNK_SUBSCRIPTION_ID)
         run(
             trajectory_cocip_bq_publisher=trajectory_cocip_bq_publisher,
             job_handler=job_handler,
