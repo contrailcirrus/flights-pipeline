2dfa4e56817820e6b6d8c724523094307de082ff
Maximilian Czerny <mcximilian@gmail.com>
2026-02-02 15:21:35 +0100

    Add monthly partitions of main and meta tables. Create the partitions dynamically in the DB util.

--- emissions-report-datastore/sql/trajectory_cocip.sql
+++ emissions-report-datastore/sql/trajectory_cocip.sql
@@ -17,7 +17,7 @@ CREATE TABLE "trajectory-cocip"
     lon_start               real,
     lat_end                 real,
     lon_end                 real,
-    time_start              timestamp without time zone,
+    time_start              timestamp without time zone not null,
     time_end                timestamp without time zone,
     sum_ef_mj               bigint,
     ef_mj_per_km            double precision,
@@ -26,9 +26,7 @@ CREATE TABLE "trajectory-cocip"
     mean_aircraft_mass_kg   integer,
     mean_overall_efficiency real,
     icao_address            text,
-    flight_id               text not null
-        constraint "trajectory-cocip_pk"
-            primary key,
+    flight_id               text not null,
     callsign                text,
     tail_number             text,
     flight_number           text,
@@ -38,8 +36,11 @@ CREATE TABLE "trajectory-cocip"
 
     flight_length_bucket    flight_length_bucket_enum,
     co2e_kg_bucket          co2e_bucket_enum,
-    co2e_kg_per_km_bucket   co2e_bucket_enum
-);
+    co2e_kg_per_km_bucket   co2e_bucket_enum,
+
+    -- Must include time_start for partitioning
+    CONSTRAINT "trajectory-cocip_pk" PRIMARY KEY (flight_id, time_start)
+) PARTITION BY RANGE (time_start);
 
 -- Given that filters are optional any combination of them can be provided making standard B-Tree indices useless.
 -- Using a GIN index which efficiently computes intersections of any filter combinations.
@@ -70,11 +71,16 @@ CREATE INDEX idx_sort_time
 CREATE INDEX idx_arr_sort_time ON "trajectory-cocip" (arrival_airport_icao, time_start DESC);
 CREATE INDEX idx_dep_sort_time ON "trajectory-cocip" (departure_airport_icao, time_start DESC);
 
-CREATE INDEX idx_dep_sort_impact ON "trajectory-cocip" (departure_airport_icao, sum_ef_mj DESC);
-CREATE INDEX idx_arr_sort_impact ON "trajectory-cocip" (arrival_airport_icao, sum_ef_mj DESC);
+CREATE INDEX idx_dep_sort_impact ON "trajectory-cocip" (departure_airport_icao, sum_ef_mj DESC, time_start DESC);
+CREATE INDEX idx_arr_sort_impact ON "trajectory-cocip" (arrival_airport_icao, sum_ef_mj DESC, time_start DESC);
 
-CREATE INDEX idx_dep_sort_intensity ON "trajectory-cocip" (departure_airport_icao, ef_mj_per_km DESC);
-CREATE INDEX idx_arr_sort_intensity ON "trajectory-cocip" (arrival_airport_icao, ef_mj_per_km DESC);
+CREATE INDEX idx_dep_sort_intensity ON "trajectory-cocip" (departure_airport_icao, ef_mj_per_km DESC, time_start DESC);
+CREATE INDEX idx_arr_sort_intensity ON "trajectory-cocip" (arrival_airport_icao, ef_mj_per_km DESC, time_start DESC);
+
+CREATE INDEX idx_airline_flight_length_sort_impact
+    ON "trajectory-cocip" (airline_iata, flight_length_bucket, sum_ef_mj DESC, time_start DESC);
+CREATE INDEX idx_flight_length_sort_impact
+    ON "trajectory-cocip" (flight_length_bucket, sum_ef_mj DESC, time_start DESC);
 
 alter table "trajectory-cocip" owner to postgres;
 grant delete, insert, select, update on "trajectory-cocip" to internal_user_rw;
