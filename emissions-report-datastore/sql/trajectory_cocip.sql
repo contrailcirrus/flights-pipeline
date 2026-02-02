24c5e82e39257a3c0658900ba329636cb5c0bd90
Maximilian Czerny <mcxme@users.noreply.github.com>
2026-02-02 09:56:45 +0100

    Precompute discrete filters in database (#427)
    * Precompute discrete range filters in database and create dynamic GIN index
    
    * Compute generated rows in db util for more flexibility and write metadata table
    
    * Fix bin label bug and avoid DataFrame deep copy overhead
    
    * Fix data types and copy on view assignment warnings
    
    * Fix materialized view feature names
    
    ---------
    
    Co-authored-by: Maximilian Czerny <mcximilian@gmail.com>

--- emissions-report-datastore/sql/trajectory_cocip.sql
+++ emissions-report-datastore/sql/trajectory_cocip.sql
@@ -1,13 +1,26 @@
-create table "trajectory-cocip"
+CREATE TYPE flight_length_bucket_enum AS ENUM (
+    'short_flight',
+    'long_flight'
+);
+
+CREATE TYPE co2e_bucket_enum AS ENUM (
+    'no_impact',
+    'low_impact',
+    'medium_impact',
+    'high_impact'
+);
+
+CREATE TABLE "trajectory-cocip"
 (
     chunk_len_km            smallint,
     lat_start               real,
     lon_start               real,
     lat_end                 real,
     lon_end                 real,
-    time_start              timestamp,
-    time_end                timestamp,
+    time_start              timestamp without time zone,
+    time_end                timestamp without time zone,
     sum_ef_mj               bigint,
+    ef_mj_per_km            double precision,
     aircraft_type_icao      text,
     engine_uid              text,
     mean_aircraft_mass_kg   integer,
@@ -23,25 +36,42 @@ create table "trajectory-cocip"
     departure_airport_icao  text,
     arrival_airport_icao    text,
 
-    ef_mj_per_km double precision GENERATED ALWAYS AS (
-        CASE WHEN chunk_len_km = 0 THEN NULL ELSE sum_ef_mj::double precision / chunk_len_km END
-    ) STORED,
-    duration_mins smallint GENERATED ALWAYS AS (
-        EXTRACT(EPOCH FROM (time_end - time_start)) / 60) STORED,
+    flight_length_bucket    flight_length_bucket_enum,
+    co2e_kg_bucket          co2e_bucket_enum,
+    co2e_kg_per_km_bucket   co2e_bucket_enum
 );
 
-CREATE INDEX idx_ef_desc
-    ON "trajectory-cocip" (sum_ef_mj DESC, time_start);
-CREATE INDEX idx_ef_per_km_desc
-    ON "trajectory-cocip" (ef_mj_per_km DESC, time_start);
-CREATE INDEX idx_duration_mins
-    ON "trajectory-cocip" (duration_mins);
-CREATE INDEX index_time_start_time_end
-    ON "trajectory-cocip" (time_start, time_end);
-CREATE INDEX idx_airline_time_start_ef
-    ON "trajectory-cocip" (airline_iata, sum_ef_mj DESC, time_start);
-CREATE INDEX idx_airline_time_start_ef_per_km
-    ON "trajectory-cocip" (airline_iata, ef_mj_per_km DESC, time_start);
+-- Given that filters are optional any combination of them can be provided making standard B-Tree indices useless.
+-- Using a GIN index which efficiently computes intersections of any filter combinations.
+CREATE EXTENSION IF NOT EXISTS btree_gin;
+CREATE INDEX idx_trajectory_filters_gin
+    ON inventory_monthly_airlines_stats
+    USING GIN (
+        airline_iata,
+        aircraft_type_icao,
+        engine_uid,
+        flight_len_bucket,
+        co2e_impact_bucket,
+        co2e_intensity_bucket,
+        departure_airport_icao,
+        arrival_airport_icao
+    );
+
+-- Add indices for sort options (and add time_start for deterministic sorting on equal values).
+CREATE INDEX idx_sort_total_impact
+    ON "trajectory-cocip" (sum_ef_mj DESC, time_start DESC);
+CREATE INDEX idx_sort_per_km_impact
+    ON "trajectory-cocip" (ef_mj_per_km DESC, time_start DESC);
+CREATE INDEX idx_sort_time
+    ON "trajectory-cocip" (time_start DESC, time_end DESC);
+
+-- Create indices that include aggregation data for faster lookups.
+CREATE INDEX idx_arrival_time_covering
+    ON "trajectory-cocip" (arrival_airport_icao, time_start)
+    INCLUDE (sum_ef_mj, chunk_len_km);
+CREATE INDEX idx_departure_time_covering
+    ON "trajectory-cocip" (departure_airport_icao, time_start)
+    INCLUDE (sum_ef_mj, chunk_len_km);
 
 alter table "trajectory-cocip" owner to postgres;
 grant delete, insert, select, update on "trajectory-cocip" to internal_user_rw;
