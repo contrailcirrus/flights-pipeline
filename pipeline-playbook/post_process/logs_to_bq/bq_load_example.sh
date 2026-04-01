42f08271d62831d8dec11ea00f7a4dcf25ed588f
Joffrey Peters <joffrey.peters@contrails.org>
2026-03-31 22:41:32 -0400

    Updates to twjd_logs_bq_schema.json and bq load example script for Mar2026 run.

--- pipeline-playbook/post_process/logs_to_bq/bq_load_example.sh
+++ pipeline-playbook/post_process/logs_to_bq/bq_load_example.sh
@@ -12,18 +12,14 @@
 # if exactly all logs for a given run can't be referenced with a single glob
 # then this command may need to be executed several times over a list of globs
 # that completely and exactly target logs for a given run
-RUN1_URI_GLOB="gs://contrails-301217-sandbox-internal/flights-pipeline/inventory_2024_run_feb2026/twjf-logs/run1/"
-RUN2_URI_GLOB="gs://contrails-301217-sandbox-internal/flights-pipeline/inventory_2024_run_feb2026/twjf-logs/run2/"
+RUN_URI_GLOB="gs://contrails-301217-flights-pipeline-prod/logs/inventory_2024-2025_run_mar2026/twjf-logs/"
 
 # TARGET TABLE
 # ---
 # table in BigQuery where the raw TWJF logs are uploaded
 # -
 # format should be similar to twjf_{inventory_period}_logs_{run_date}
-TARGET_TABLE="twjf_2024_logs_feb2026"
+TARGET_TABLE="twjf_2024-2025_logs_mar2026"
 
 # For each file in RUN1_URI (non-null airline_iata), load data  into BQ table 
-gsutil ls -r $RUN1_URI_GLOB | grep .json | tr '\n' '\0' | xargs -0 -n1 bq load --schema_update_option=ALLOW_FIELD_ADDITION --ignore_unknown_values --source_format=NEWLINE_DELIMITED_JSON --schema=./twjd_logs_bq_schema_lean.json flights_pipeline_prod.${TARGET_TABLE}
-
-# For each file in RUN2_URI (null airline_iata), load data  into BQ table 
-gsutil ls -r $RUN2_URI_GLOB | grep .json | tr '\n' '\0' | xargs -0 -n1 bq load --schema_update_option=ALLOW_FIELD_ADDITION --ignore_unknown_values --source_format=NEWLINE_DELIMITED_JSON --schema=./twjd_logs_bq_schema_lean.json flights_pipeline_prod.${TARGET_TABLE}
+gsutil ls -r $RUN_URI_GLOB | grep .json | tr '\n' '\0' | xargs -0 -n1 bq load --schema_update_option=ALLOW_FIELD_ADDITION --ignore_unknown_values --source_format=NEWLINE_DELIMITED_JSON --schema=./twjd_logs_bq_schema.json flights_pipeline_prod.${TARGET_TABLE}
